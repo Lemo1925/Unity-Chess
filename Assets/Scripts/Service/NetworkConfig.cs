99a53c9606e8e75c4c06447de1171fdb697ce952
Lemo <996860039@qq.com>
2023-08-23 23:55:09 +0800

    TCP and Serializable

---
+++ Assets/Scripts/Service/NetworkConfig.cs
@@ -0,0 +1,8 @@
+﻿
+using UnityEngine;
+
+public class NetworkConfig : MonoBehaviour
+{
+    public const string SERVER_IP = "127.0.0.1";
+    public const int PORT = 4567;
+}
---
+++ Assets/Scripts/Utils/SerializableObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerializableObject : MonoBehaviour
+{
+    [Serializable]
+    public class Node
+    {
+        public List<Selection> selections = new List<Selection>();
+        public List<ChessType> chessGOKey = new List<ChessType>();
+        public List<GameObject> chessGOValue = new List<GameObject>();
+    }
+
+    private static Node node = new Node();
+    
+    public void Initialize()
+    {
+        foreach (var keyValuePair in ChessBoard.instance.chessGO)
+        foreach (var Object in keyValuePair.Value)
+        {
+            node.chessGOKey.Add(keyValuePair.Key);
+            node.chessGOValue.Add(Object);
+        }
+
+        for (var i = 0; i < 8; i++)
+        for (var j = 0; j < 8; j++)
+            node.selections.Add(ChessBoard.instance.ChessSelections[i, j]);
+    }
+
+    public static Dictionary<ChessType, List<GameObject>> deserializationChessGO()
+    {
+        var chessGO = new Dictionary<ChessType, List<GameObject>>();
+        for (var i = 0; i < node.chessGOKey.Count; i++) 
+            chessGO[node.chessGOKey[i]].Add(node.chessGOValue[i]);
+        return chessGO;
+    }
+
+    public static Selection[,] deserializationSelections()
+    {
+        var ChessSelections = new Selection[8, 8];
+        for (var i = 0; i < 8; i++)
+        for (var j = 0; j < 8; j++)
+            ChessSelections[i, j] = node.selections[i * 7 + j];
+        return ChessSelections;
+    }
+}
